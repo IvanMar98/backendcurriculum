13b1904e9ecbaf0ecee8150def636b5f91c9a0a8
Ivan <ivan18.im67@gmail.com>
2022-08-09 23:41:16 -0500

    Modelo divisiones

--- app/database/mysql.js
+++ app/database/mysql.js
@@ -1,6 +1,7 @@
 const sequelize = require('./conexiondb'); 
 const Apoyo = require('./tablas/apoyo');
 const CuerpoAcademico = require('./tablas/cuerpo_academico');
+const Divisiones = require('./tablas/divisiones');
 const Pais = require('./tablas/pais');
 init = function(){
     sequelize.authenticate().then(()=>{
@@ -9,6 +10,8 @@ init = function(){
         console.log("conexion no establecida", err);
     })
 } 
+
+
 
 getPais = function(callback){
     Pais.findAll().then(pais => callback(pais));
@@ -19,8 +22,15 @@ getApoyo = function(callback){
 getCuerpoAcademico = function(callback){
     CuerpoAcademico.findAll().then(cuerpo_academico => callback(cuerpo_academico));
 }
+getDivisiones = function(callback){
+    Divisiones.findAll().then(divisiones => callback)
+}
 module.exports.init = init;  
+
+
 
 module.exports.getPais = getPais; 
 module.exports.getApoyo = getApoyo;
 module.exports.getCuerpoAcademico = getCuerpoAcademico;
+module.exports.getDivisiones = getDivisiones;
+
